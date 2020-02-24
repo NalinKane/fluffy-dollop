2d5fd7f457b9c05b09a3df1b26416a8d2eca00a2
Anna Lebiedziewicz <nalin.kane@gmail.com>
2020-02-24 18:22:09 +0000

    Add form to search for employees

--- src/components/LookupHeader.jsx
+++ src/components/LookupHeader.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const LookupHeader = () => {
-  return <div>LookupHeader</div>;
+  return (
+    <form>
+      <div className="form-row">
+        <div className="form-group col-md-8">
+          <label htmlFor="inputCity">Search term</label>
+          <input type="text" className="form-control" id="inputCity" />
+        </div>
+        <div className="form-group col-md-4">
+          <label htmlFor="inputState">Filter by</label>
+          <select id="inputState" className="form-control">
+            <option value="">Choose...</option>
+          </select>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default LookupHeader;
